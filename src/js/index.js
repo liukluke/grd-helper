d03142a593d5aff6fad6f60e8bacc9695d069c4b
lmanfredi <lmanfredi@minsait.com>
2020-05-13 10:54:05 +0200

    init project

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import 'reset-css'
+import '../assets/scss/main.scss'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/js/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/view/index.pug'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(sc|c)ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            },
+            {
+                test: /\.(png|jpeg|woff|woff2|eot|ttf|svg)$/, 
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname + '/public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public'
+    }
+}
